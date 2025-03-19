43173289e53eac56851f730359e0d977f696d751
marioterron <marioterron157@gmail.com>
2025-03-19 17:53:09 +0100

    refactor(metadata): consolidate home metadata into layout file

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,6 +1,40 @@
-import { homeMetadata } from "../metadata";
+import { Metadata } from "next";
+import { WEDDING_DETAILS } from "@/constants/wedding";
 
-export const metadata = homeMetadata;
+export const metadata: Metadata = {
+  title: `${WEDDING_DETAILS.couple.person1} & ${WEDDING_DETAILS.couple.person2}'s Wedding - September 6th, 2025`,
+  description: `You're invited to celebrate the wedding of ${WEDDING_DETAILS.couple.person1} and ${WEDDING_DETAILS.couple.person2} on September 6th, 2025 at ${WEDDING_DETAILS.venue.name}. Join us for this special celebration of love in ${WEDDING_DETAILS.venue.city}, ${WEDDING_DETAILS.venue.country}.`,
+  openGraph: {
+    title: `${WEDDING_DETAILS.couple.person1} & ${WEDDING_DETAILS.couple.person2}'s Wedding`,
+    description: `Join us in celebrating the wedding of ${WEDDING_DETAILS.couple.person1} and ${WEDDING_DETAILS.couple.person2} on September 6th, 2025 at ${WEDDING_DETAILS.venue.name}, ${WEDDING_DETAILS.venue.city}, ${WEDDING_DETAILS.venue.country}.`,
+    images: [
+      {
+        url: "/images/image-center.jpeg",
+        width: 1200,
+        height: 630,
+        alt: `${WEDDING_DETAILS.couple.person1} and ${WEDDING_DETAILS.couple.person2}'s Wedding Invitation`,
+      },
+      {
+        url: "/images/location.jpg",
+        width: 1200,
+        height: 630,
+        alt: `${WEDDING_DETAILS.venue.name} - Wedding Venue`,
+      },
+    ],
+  },
+  keywords: [
+    "wedding invitation",
+    "wedding celebration",
+    "wedding ceremony",
+    WEDDING_DETAILS.venue.name,
+    WEDDING_DETAILS.venue.city,
+    WEDDING_DETAILS.venue.country,
+    WEDDING_DETAILS.couple.person1,
+    WEDDING_DETAILS.couple.person2,
+    "wedding venue",
+    "wedding details",
+  ],
+};
 
 export default function HomeLayout({
   children,
