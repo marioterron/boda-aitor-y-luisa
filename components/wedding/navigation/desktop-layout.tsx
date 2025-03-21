a58eb8fd92f8c660684715dac09b614ebd0af2b2
marioterron <marioterron157@gmail.com>
2025-03-21 16:38:31 +0100

    refactor(navigation): implement i18n translations

--- components/wedding/navigation/desktop-layout.tsx
+++ components/wedding/navigation/desktop-layout.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import { useTranslations } from "next-intl";
 
 import { Button } from "@/components/ui/button";
+import { WEDDING_DETAILS } from "@/constants/wedding";
 import { NavLinks } from "./nav-links";
 
 interface DesktopLayoutProps {
@@ -8,11 +10,18 @@ interface DesktopLayoutProps {
 }
 
 export function DesktopLayout({ scrollToRSVP }: DesktopLayoutProps) {
+  const t = useTranslations("navigation");
+
   return (
     <div className="hidden md:block mx-auto px-4">
       <div className="relative flex h-20 items-center justify-between">
-        <h1 className="absolute left-1/2 -translate-x-1/2 whitespace-nowrap font-serif text-xl md:text-2xl">
-          <Link href="/">AITOR & LUISA</Link>
+        <h1 className="uppercase absolute left-1/2 -translate-x-1/2 whitespace-nowrap font-serif text-xl md:text-2xl">
+          <Link href="/">
+            {t("home", {
+              groom: WEDDING_DETAILS.couple.groom,
+              bride: WEDDING_DETAILS.couple.bride,
+            })}
+          </Link>
         </h1>
         <NavLinks className="flex items-center space-x-8 " />
         <div className="flex items-center">
@@ -22,7 +31,7 @@ export function DesktopLayout({ scrollToRSVP }: DesktopLayoutProps) {
             size="sm"
             className="uppercase"
           >
-            RSVP
+            {t("rsvp")}
           </Button>
         </div>
       </div>
--- components/wedding/navigation/mobile-layout.tsx
+++ components/wedding/navigation/mobile-layout.tsx
@@ -1,7 +1,9 @@
 import { Menu, X } from "lucide-react";
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
+import { WEDDING_DETAILS } from "@/constants/wedding";
 import { NavLinks } from "./nav-links";
 
 interface MobileLayoutProps {
@@ -15,11 +17,18 @@ export function MobileLayout({
   setIsMenuOpen,
   scrollToRSVP,
 }: MobileLayoutProps) {
+  const t = useTranslations("navigation");
+
   return (
     <div className="mx-auto px-4">
       <div className="relative flex h-20 items-center justify-between">
-        <h1 className="absolute left-1/2 -translate-x-1/2 whitespace-nowrap font-serif text-xl md:text-2xl">
-          <Link href="/">AITOR & LUISA</Link>
+        <h1 className="uppercase absolute left-1/2 -translate-x-1/2 whitespace-nowrap font-serif text-xl md:text-2xl">
+          <Link href="/">
+            {t("home", {
+              groom: WEDDING_DETAILS.couple.groom,
+              bride: WEDDING_DETAILS.couple.bride,
+            })}
+          </Link>
         </h1>
         <button
           onClick={() => setIsMenuOpen(!isMenuOpen)}
@@ -32,7 +41,6 @@ export function MobileLayout({
             <Menu className="h-6 w-6 transition-all duration-300" />
           )}
         </button>
-
         <div
           className={`fixed inset-x-0 top-20 bottom-0 bg-white/95 transition-all duration-300 ${
             isMenuOpen ? "visible opacity-100" : "invisible opacity-0"
@@ -56,7 +64,7 @@ export function MobileLayout({
                 size="lg"
                 className="text-2xl uppercase"
               >
-                RSVP
+                {t("rsvp")}
               </Button>
             </div>
           </div>
