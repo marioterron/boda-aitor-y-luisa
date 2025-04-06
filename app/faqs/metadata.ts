1b592406744d34d33c11d367c2aa2c9f23adfb1d
marioterron <marioterron157@gmail.com>
2025-04-06 11:35:23 +0200

    fix(metadata): update couple name formatting to use Spanish conjunction for consistency

--- app/faqs/metadata.ts
+++ app/faqs/metadata.ts
@@ -3,7 +3,7 @@ import { Metadata } from "next";
 import { WEDDING_DETAILS } from "@/constants/wedding";
 import messages from "@/messages/es.json";
 
-const couple = `${WEDDING_DETAILS.couple.groom} and ${WEDDING_DETAILS.couple.bride}`;
+const couple = `${WEDDING_DETAILS.couple.groom} y ${WEDDING_DETAILS.couple.bride}`;
 const metadataMessages = messages.metadata;
 const commonMessages = messages.common;
 
